90e7bedeae610f4f4f537295586e8b65f270192a
augustine <augustino0890@gmail.com>
2021-02-09 08:07:44 +0000

    .gitignore

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create the virtual environment
+python -m venv .env
+# download requirements
+.env/bin/python -m pip install -r requirements.txt
+# create the key pair
+aws ec2 create-key-pair --key-name key_pair --query 'KeyMaterial' --output text > key_pair.pem --region us-east-2
+# update key_pair permissions
+chmod 400 key_pair.pem
+# move key_pair to .ssh
+mv -f key_pair.pem $HOME/.ssh/key_pair.pem
+# start the ssh agent
+eval `ssh-agent -s`
+# add your key to keychain
+ssh-add -k ~/.ssh/key_pair.pem
